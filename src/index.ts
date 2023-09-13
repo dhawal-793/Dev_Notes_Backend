72077f5bbe53867c495785804cbd61810eda3526
Dhawal Vijayvargiya <88284372+dhawal-793@users.noreply.github.com>
2023-09-13 16:19:15 +0530

    Created simple express server with port 5000.

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,17 @@
-console.log("Hello World");
+import { Application, NextFunction, Request, Response } from "express";
+
+const express = require("express")
+
+const app: Application = express();
+const PORT = process.env.PORT || 5000;
+
+
+app.use("/", (req: Request, res: Response, next: NextFunction) => {
+    res.status(200).send({ data: "Hello World" })
+})
+
+
+app.listen(PORT, () => {
+    console.log(`Server is Running on port ${PORT}`);
+
+})
