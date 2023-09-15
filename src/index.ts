8e1d2d54c98c41183fad2c1d77fd3a4d76b810c5
Dhawal Vijayvargiya <88284372+dhawal-793@users.noreply.github.com>
2023-09-15 21:53:32 +0530

    Updated app to use cors.

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import express from 'express'
+import cors from 'cors'
 import type { Application, NextFunction, Request, Response } from 'express'
 
 import './Utils/connectToDB'
@@ -6,6 +7,12 @@ import { routes } from './routes'
 
 const app: Application = express()
 const PORT: number = 5000
+
+// cors access handler
+app.use(cors())
+app.use(express.json())
+// app.use(bodyParser.urlencoded({ extended: false }))
+// app.use(bodyParser.json())
 
 app.get('/', (req: Request, res: Response, next: NextFunction) => {
   res.status(200).send({ data: 'Hello World' })
